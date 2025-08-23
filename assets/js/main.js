ca14cfd5622c4cb605a75d5f4777903d952dfb45
lemanois56 <lemanolivier7@gmail.com>
2025-08-23 02:47:45 +0000

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,7 +6,7 @@
  * (l'historique est conservé).
  */
 const SOLDE_INITIAL = 114500443;
-const FORCE_RESET_SOLDE = true;
+const FORCE_RESET_SOLDE = false;
 /** ===================================================== */
 /*__INIT_LOCAL_BLOCK__*/
 (function initLocal() {
